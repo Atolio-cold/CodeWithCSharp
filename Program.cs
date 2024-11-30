97ac44e77c44026f8138a3d3ae52033128b69d7d
Antoni <uchanskiantoni@gmail.com>
2024-11-30 17:10:38 +0100

    Add easy program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Globalization;
+
+namespace NumberGuesser
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Insert number: ");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int? maxValue = null;
+
+
+
+            while (userInput != 0)
+            {
+                if (maxValue == null || userInput > maxValue)
+                {
+                    maxValue = userInput;
+
+                }
+                sum += userInput;
+
+                userInput = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine($"Sum of numbers = {sum}");
+            if (maxValue != null)
+            {
+                Console.WriteLine($"MaxValue = {maxValue}");
+
+            }
+            else
+            {
+                Console.WriteLine("MaxValue isn't correct");
+            }
+
+
+        }
+    }
+}
